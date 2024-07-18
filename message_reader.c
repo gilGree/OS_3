073dcf860ca143def6997b2e4e6f63ff24d51ec4
gilGree <gilg1@mail.tau.ac.il>
2024-07-18 20:34:42 +0300

    hel

---
+++ message_reader.c
@@ -0,0 +1,43 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "message_slot.h"
+
+#define MAJOR_NUM 235
+#define BUF_LEN 128
+#define MSG_SLOT_CHANNEL _IOW(MAJOR_NUM, 0, unsigned int)
+
+int main(int argc, char const *argv[]) {
+    int file_desc, len_message;
+    unsigned int channel_id;
+    char buffer_for_message[BUF_LEN];
+
+    if (argc != 3) {
+        perror("the number of arguments must be two");
+        exit(1);
+    }
+    file_desc = open(argv[1], O_RDONLY);
+    if (file_desc == -1) {
+        perror("failed to open device file \n");
+        exit(1);
+    }
+    channel_id = atoi(argv[2]);
+    if (ioctl(file_desc, MSG_SLOT_CHANNEL, channel_id) < 0){
+        perror("failed to set the channel id \n");
+        exit(1);
+    }
+    len_message = read(file_desc, buffer_for_message, BUF_LEN);
+    if (len_message < 0) {
+        perror("failed to read from message slot file \n");
+        exit(1);
+    }
+    close(file_desc);
+    if (write(1, buffer_for_message, len_message) != len_message) {
+        perror("failed to print the message\n");
+        exit(1);
+    }
+    exit(0);
+}
---
+++ message_sender.c
@@ -0,0 +1,38 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "message_slot.h"
+
+#define MAJOR_NUM 235
+#define MSG_SLOT_CHANNEL _IOW(MAJOR_NUM, 0, unsigned int)
+
+int main(int argc, char const *argv[]) {
+    int file_desc, len_message;
+    unsigned int channel_id;
+    
+    if (argc != 4) {
+        perror("the number of arguments must be three");
+        exit(1);
+    }
+    file_desc = open(argv[1], O_WRONLY);
+    if (file_desc == -1) {
+        perror("failed to open device file \n");
+        exit(1);
+    }
+    channel_id = atoi(argv[2]);
+    if (ioctl(file_desc, MSG_SLOT_CHANNEL, channel_id) < 0){
+        perror("failed to set the channel id \n");
+        exit(1);
+    }
+    len_message = strlen(argv[3]);
+    if (write(file_desc, argv[3], len_message) != len_message) {
+        perror("failed to write the message to the slot file \n");
+        exit(1);
+    }
+    close(file_desc);
+    exit(0);
+}
